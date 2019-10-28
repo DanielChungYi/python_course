39d0ec2726c1623a6650e6b7867537d052190c74
DanielChungYi <31957705+DanielChungYi@users.noreply.github.com>
2019-10-28 17:17:40 +0800

    re split && plot

--- test_learn_rate/read_result.py
+++ test_learn_rate/read_result.py
@@ -3,6 +3,8 @@ import argparse
 import pandas as pd
 import numpy as np
 import operator
+import re
+import matplotlib.pyplot as plt
 #fout.close() 
 dirs = os.listdir("/home/user/daniel_ws/yolov3/result")
 os.chdir("/home/user/daniel_ws/yolov3/result")
@@ -14,17 +16,35 @@ print(best)
 for file in dirs:
     #print(file)
     df = pd.read_csv(file)
-    df_ = df.as_matrix()
-    x = np.array(df_)
-    for i in x[8]:
-        z = i.split("    ",15)
-        #print(z[11])
-        best.update({file : float(z[11])})
+    x = df.as_matrix()
+    #x = np.array(df_)
+    #print(x[8])
+    z = re.split(" +",str(x[8]))
+    #print(z)
+    best.update({file : float(z[11])})
 #print(best)
 
 best_lr = max(best.items(), key=operator.itemgetter(1))[0]
 print(best_lr)
+x_plot = np.array([])
+y_plot = np.array([])
+
+for key, value in best.items():
+    x_plot_temp = key.split('_')
+    x_plot_temp = x_plot_temp[1].rsplit('.',1)
+    x_plot = np.append(x_plot , float(x_plot_temp[0]))
+    y_plot = np.append(y_plot , float(value))
+    print(key)
+    print(value)
+
+print(x_plot)
+print(y_plot)
 
 
-#open(file)  或是 pd.read_csv(file) 讀進來的每一列都為string
-#用 split把每一列當中的每個 column 分開
+fig, ax = plt.subplots(figsize=(8, 4))
+ax.scatter(x_plot, y_plot)
+fig.suptitle('random lr vs mAP (10 epoch)')
+plt.autoscale(enable=True, axis='both', tight=None)
+plt.xlabel('learning rate')
+plt.ylabel('mAP')
+plt.show()
